c053de3781365b4ca9ccdbc437c8bf83e124a503
Dom <domthydang@gmail.com>
2021-02-21 12:07:55 -0500

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Exoplanets from "./pages/Exoplanets";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 import NotFound from "./pages/NotFound";
 import "./App.css";
 
 const App = () => {
     return (
         <div className="page-container">
-           {/* <Navbar /> */}
-        <div className = "content-wrap">
-            <Router>
-                <Switch>
-                    <Route path="/" exact>
-                        <Home />
-                    </Route>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/exoplanets">
-                        <Exoplanets />
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
+            <Navbar />
+            <div className="content-wrap">
+                <Router>
+                    <Switch>
+                        <Route path="/" exact>
+                            <Home />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/exoplanets">
+                            <Exoplanets />
+                        </Route>
+                        <Route path="*">
+                            <NotFound />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
             <Footer />
         </div>
     );
