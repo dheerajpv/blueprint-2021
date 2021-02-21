869a9b1cbd638f6e9fba6cf70ac64cebd90c70ca
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-02-21 17:11:33 -0500

    whatever this is

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,13 +3,13 @@ import "./Navbar.css";
 
 const Navbar = ({ page }) => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
                     Page Name thing
                 </a>
                 <button
-                    class="navbar-toggler"
+                    className="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent"
@@ -17,13 +17,13 @@ const Navbar = ({ page }) => {
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div
-                    class="collapse navbar-collapse"
+                    className="collapse navbar-collapse"
                     id="navbarSupportedContent"
                 >
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li>
                             <a
                                 href="/"
