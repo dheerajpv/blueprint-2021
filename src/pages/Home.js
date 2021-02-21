6c5eeaf6283d57dc471f6095236321f6915a76ec
Dom <domthydang@gmail.com>
2021-02-21 07:17:34 -0500

    test pt 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            {/* Here's a change */}
-            <p>Test</p>
+            <p><center>Exoplanet Graphs</center></p>
         </div>
     );
 };
