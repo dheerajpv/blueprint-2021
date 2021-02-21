e3849dc9bd4ead616c2e843468ec720cc4aa4c3b
Dom <domthydang@gmail.com>
2021-02-21 09:13:07 -0500

    started html stuff

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, Request, Response
 from flask_cors import CORS
 from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
 import io
 import base64
+import csv
 
 app = Flask(__name__)
 CORS(app)
+
+def extend_with_extra_elements(*some_lists):
+    max_some_lists_length = max(map(len, some_lists))
+    for some_list in some_lists:
+        extra_elements_count = max_some_lists_length - len(some_list)
+        extra_elements = [0] * extra_elements_count
+        yield some_list + extra_elements
 
 
 data = pd.read_csv("src/PS_2021.02.18_14.17.39.csv", low_memory=False)
@@ -20,7 +29,28 @@ def exist():
 @app.route("/graph", methods=["GET"])
 def make_graph():
     s = io.BytesIO()
-    plt.plot(list(range(100)))
+    col1 = []
+    col2 = []
+    
+    # makes col1 and col2 have a list of values from the csv
+    with open("src/PS_2021.02.18_14.17.39.csv", newline="") as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            try:
+                col1.append(float(row[74]))
+            except:
+                pass
+
+            try:
+                col2.append(float(row[75]))
+            except:
+                pass
+
+    col1, col2 = extend_with_extra_elements(col1, col2)
+
+    plt.plot(col1[1:], col2[1:], "bo", antialiased=True)
+
     plt.savefig(s, format='png', bbox_inches="tight")
     plt.close()
     s = base64.b64encode(s.getvalue()).decode("utf-8").replace("\n", "")
--- src/pages/Exoplanets.js
+++ src/pages/Exoplanets.js
@@ -4,6 +4,19 @@ const Exoplanets = () => {
     return (
         <div>
             <p><center>Astrobiology and Exoplanets</center></p>
+
+            <h2> What are exoplanets? </h2>
+            <p> Exoplanets are planets outside our Solar System. Some orbit other stars but there are free-floating exoplanets that do not. Instead, they 
+                orbit the galatic cccenter and are called rogue planets.  </p>
+            <p> 
+                Did you know that there are more planets than stars in the galaxy? This was discovered by NASA's Kepler Space Telescope.
+            </p>
+            <p>
+                The majority of discovered exoplanets are in the Milky Way Galaxy. We can find exoplanets by measuring their size and mass. Additionally, we 
+                can discover their composition whether the planet is really rocky (like Earth) or very gaseous (like Jupiter). Although they are similar in composition to 
+                our own planets, the combination of basic elements are different. For example, some planets could be filled with water or ice but others could be 
+                predominantly made of iron or carbon.  
+            </p>
         </div>
     );
 };
