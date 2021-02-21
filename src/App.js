ed0db694edef10077c9dba38b87352c977e70062
Dom <domthydang@gmail.com>
2021-02-21 06:54:53 -0500

    let's see

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
